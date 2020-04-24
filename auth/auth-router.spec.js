befed990cb71a8e8dc69d5081dcab289253fa986
emma <sonicjane2020@gmail.com>
2020-04-24 10:35:51 -0700

    final mvp

--- auth/auth-router.spec.js
+++ auth/auth-router.spec.js
@@ -3,13 +3,15 @@ const db = require('../database/dbConfig');
 const Users = require('../auth/auth-model');
 const server = require ('../api/server');
 
+let token;
+
 describe("tests login and register", () => {
     
     it("tests register", async () => {});
   
     describe("should test register", () => {
       it("should register user", async () => {
-        const user = { username: "bbb", password: "eee" };
+        const user = { username: "zeeeeeeeee", password: "eqeeeeeq" };
         const res = await request(server)
           .post("/api/auth/register")
           .send(user);
@@ -25,4 +27,13 @@ describe("tests login and register", () => {
       });
     });
   });
-  
+
+  describe('jokes', () => {
+    it('should require authorization', () => {
+        return request(server)
+            .get('/api/jokes')
+            .then((response) => {
+                expect(response.status).toBe(401);
+            })
+    })
+})
